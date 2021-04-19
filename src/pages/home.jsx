78c1f79a6854e7742d6692cba5f74edda397b92b
letuongqui <trinhnhatsinh123@gmail.com>
2021-04-19 09:16:18 +0700

    update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@/styles/home.css';
-import ManipulationItem from '@/components/home/manipulation_item';
+import ManipulationItem from '@/components/home/manipulation_item.jsx';
 import dataFeatures from '../contants/menu';
 import Header from '../components/header/header';
 // import { useHistory, useLocation } from 'react-router-dom';
