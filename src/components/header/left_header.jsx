ff9f638b907c7b2c08ed6b25a394c28f50704d61
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2021-04-26 17:34:42 +0700

    Manage Document Update 1

--- src/components/header/left_header.jsx
+++ src/components/header/left_header.jsx
@@ -3,12 +3,14 @@ import { useLocation } from 'react-router-dom';
 import Logo from '@/components/header/logo';
 import CreateSignature from './create_signature';
 
-const location = useLocation();
-const LeftHeader = () => (
-  <div className="wrap-header__left">
-    <Logo />
-    {location.pathname === '/' && <CreateSignature />}
-  </div>
-);
+const LeftHeader = () => {
+  const location = useLocation();
+  return (
+    <div className="wrap-header__left">
+      <Logo />
+      {location.pathname === '/' && <CreateSignature />}
+    </div>
+  );
+};
 
 export default LeftHeader;
